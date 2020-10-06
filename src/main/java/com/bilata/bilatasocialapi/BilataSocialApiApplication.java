ca4e0cf1f882e6453d23cf8348c41e3df72e39d3
ndemisse <natnael.demisse@siriusxm.com>
2020-10-05 21:39:39 -0500

    add springboot starter code

---
+++ src/main/java/com/bilata/bilatasocialapi/BilataSocialApiApplication.java
@@ -0,0 +1,13 @@
+package com.bilata.bilatasocialapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BilataSocialApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BilataSocialApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bilata/bilatasocialapi/BilataSocialApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bilata.bilatasocialapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BilataSocialApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
